12c9f3f07fe347547a3c569ee3b640b3a1f155fc
guiindiciate <gui.indiciate@outlook.com>
2023-10-24 10:39:21 -0300

    Atualização nextconfig para o cat

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    images: {
+        remotePatterns:[
+            {
+            protocol: "https",
+            hostname: "prints.ultracoloringpages.com",
+            port: "",
+            pathname: "/**"
+        }
+        ,
+        {
+            protocol: "https",
+            hostname: "http.cat",
+            port: "",
+            pathname: "/images/**"
+        }
+        ,
+    ],
+    },
+}
